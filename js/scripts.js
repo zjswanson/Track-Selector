6300b0b585997ae76cb59e7bb889cd7929d1bc6f
Zach Swanson <zach.j.swanson@gmail.com>
2017-01-13 12:45:52 -0800

    add script to return preferred language from user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 // question 1-3 are to determine if the user prefers to work for a large or small organization.  if yes, they increment preferBig.
 // Questions 4-5, respectively, refine which language to use in their preferred org size
 
-var preferSize = 0;
 var checkSize = function(q1,q2,q3) {
   if (q1+q2+q3 > 1) {
     return "big";
@@ -10,6 +9,7 @@ var checkSize = function(q1,q2,q3) {
     return "small";
   }
 };
+var preferLanguage;
 
 
 $(function() {
@@ -24,7 +24,12 @@ $(function() {
     var q1 = parseInt($("input:radio[name=question1]:checked").val());
     var q2 = parseInt($("input:radio[name=question2]:checked").val());
     var q3 = parseInt($("input:radio[name=question3]:checked").val());
-
+    if (checkSize(q1,q2,q3) === "big")  {
+      var preferLanguage = $("input:radio[name=question4]:checked").val();
+    } else {
+      var preferLanguage = $("input:radio[name=question5]:checked").val();
+    }
+    alert(preferLanguage);
   });
 
 
